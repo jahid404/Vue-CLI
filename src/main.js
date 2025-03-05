f34674b9d3e8adc6b15f880ebf73fc1c694026ce
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2025-03-05 17:14:27 +0000

    Mar 5, 2025, 11:14 PM

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'jquery'
 import 'nprogress/nprogress.css'
-import '/assets/css/styles.css'
+import '@/assets/css/styles.css'
 
 const app = createApp(App)
 app.use(router).mount('#app')
