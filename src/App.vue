0a9820d9810c7e43e31650250d2306dc7ed5840b
Jahidul Islam Jahid <jsjahidmini@gmail.com>
2025-03-09 05:33:21 +0000

    Mar 9, 2025, 11:33 AM

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
     import { RouterView, RouterLink } from 'vue-router';
     import { routeList } from './router/routes';
-    console.log("Base URL:", import.meta.env.BASE_URL);
 </script>
 
 <template>
